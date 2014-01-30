4c45a7288be6b22a9d615cd8e4638711175367c1
Simo Kinnunen <simo@shoqolate.com>
2014-01-31 05:10:38 +0900

    Load devices with Oboe.

--- res/app/scripts/controllers/DeviceListCtrl.js
+++ res/app/scripts/controllers/DeviceListCtrl.js
@@ -1,5 +1,12 @@
-define(['./module'], function(mod) {
-  mod.controller('DeviceListCtrl', ['$scope', '$http', 'devices', function($scope, $http) {
+define(['./module', 'oboe'], function(mod, oboe) {
+  mod.controller('DeviceListCtrl', ['$scope', function($scope) {
+    $scope.devices = []
 
+    oboe('/api/v1/devices')
+      .node('$devices[*]', function(devicesLoadedSoFar) {
+        $scope.$apply(function() {
+          $scope.devices = devicesLoadedSoFar
+        })
+      })
   }])
 })

077982f08a2d1b6b09e7fc3ef1f20e9f9f8984dd
Simo Kinnunen <simo@shoqolate.com>
2014-02-10 22:08:29 +0900

    Add a very lazy test for keyutil.parseKeyCharacterMap().

---
+++ test/util/keyutil.js
@@ -0,0 +1,27 @@
+var path = require('path')
+var fs = require('fs')
+
+var chai = require('chai')
+var expect = chai.expect
+
+var keyutil = require('../../lib/util/keyutil')
+
+describe('keyutil', function() {
+
+  describe('parseKeyCharacterMap', function() {
+
+    it('should be able to parse Virtual.kcm', function(done) {
+      var expected = require('../fixt/Virtual.kcm.json')
+        , source = path.join(__dirname, '..', 'fixt', 'Virtual.kcm')
+
+      keyutil.parseKeyCharacterMap(fs.createReadStream(source))
+        .then(function(keymap) {
+          expect(keymap).to.eql(expected)
+          done()
+        })
+        .catch(done)
+    })
+
+  })
+
+})

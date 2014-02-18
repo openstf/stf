8e674928eab193fa7dae1c54d108a3770428dbed
Gunther Brunner <gunta85@gmail.com>
2014-02-18 14:48:34 +0900

    Fast rebuilds.

--- res/app/app.js
+++ res/app/app.js
@@ -1,18 +1,16 @@
 require('angular')
 require('angular-route')
 
-var app = angular.module('app', [
+angular.module('app', [
   'ngRoute',
   require('./components/device-list').name
 ])
-
-
-app.config(['$routeProvider', '$locationProvider',
-  function ($routeProvider, $locationProvider) {
-    $locationProvider.hashPrefix('!');
-    $routeProvider
-      .otherwise({
-        redirectTo: '/devices'
-      })
-  }
-])
+  .config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+      $locationProvider.hashPrefix('!');
+      $routeProvider
+        .otherwise({
+          redirectTo: '/devices'
+        })
+    }
+  ])

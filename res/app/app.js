5bba97de78ecc80bd5c57cba86eb44f6ef87bc0c
Gunther Brunner <gunta85@gmail.com>
2014-03-10 20:16:18 +0900

    Adding back more CSS.

--- res/app/app.js
+++ res/app/app.js
@@ -7,12 +7,20 @@ require('style!raw!se7en-bootstrap-3/build/stylesheets/style.css')
 require('style!raw!se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')
 
 
+// Temporary here:
+require('fa-borderlayout')
+require('fa-borderlayout/build-0.3.1/borderLayout.css')
+require('fa-borderlayout/build-0.3.1/stf-style.css')
+
+
+
 
 
 angular.module('app', [
   'ngRoute',
   require('./device-list').name,
-  require('./device-control').name
+  require('./device-control').name,
+  'fa.directive.borderLayout'
 ])
   .config(['$routeProvider', '$locationProvider',
     function ($routeProvider, $locationProvider) {

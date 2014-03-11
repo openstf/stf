ca401d7b9c1034b3ccc2371c59973933a3063221
Gunther Brunner <gunta85@gmail.com>
2014-03-11 20:15:18 +0900

    - Adding loaders for images/fonts. - More styles working now.

--- res/app/layout/index.js
+++ res/app/layout/index.js
@@ -1,10 +1,9 @@
-require('style!raw!se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
-require('style!raw!se7en-bootstrap-3/build/stylesheets/se7en-font.css')
-require('style!raw!se7en-bootstrap-3/build/stylesheets/style.css')
-require('style!raw!se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')
+require('se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
+require('se7en-bootstrap-3/build/stylesheets/se7en-font.css')
+require('se7en-bootstrap-3/build/stylesheets/style.css')
+require('se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')
 
 
-// Temporary here:
 require('fa-borderlayout')
 require('fa-borderlayout/build-0.3.1/borderLayout.css')
 require('fa-borderlayout/build-0.3.1/stf-style.css')
@@ -12,7 +11,7 @@ require('fa-borderlayout/build-0.3.1/stf-style.css')
 
 require('./cursor.css')
 require('./stf-se7en.css')
-require('style!raw!./stf-styles.css')
+require('./stf-styles.css')
 require('./small.css')
 
 module.exports = angular.module('layout', [

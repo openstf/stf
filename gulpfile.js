e0006e4c5be954c742c60592aa118c26baa53cc1
Simo Kinnunen <simo@shoqolate.com>
2014-03-19 20:48:20 +0900

    Check JSON files with gulp-jsonlint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp')
 var jshint = require('gulp-jshint')
+var jsonlint = require('gulp-jsonlint')
 
-gulp.task('lint', function() {
+gulp.task('jshint', function() {
   gulp.src(['lib/**/*.js', '*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
 })
 
+gulp.task('jsonlint', function() {
+  return gulp.src(['.jshintrc', '.bowerrc', '.yo-rc.json', '*.json'])
+    .pipe(jsonlint())
+    .pipe(jsonlint.reporter())
+})
+
+gulp.task('lint', ['jshint', 'jsonlint'])
 gulp.task('test', ['lint'])

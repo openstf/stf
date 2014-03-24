29f2be60884f447db42b2eef4872f6d86c890bb1
Simo Kinnunen <simo@shoqolate.com>
2014-03-24 12:17:17 +0900

    Reformat webpack config in the one true format.

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,66 @@ var pathutil = require('./lib/util/pathutil')
 var webpack = require('webpack')
 
 module.exports = {
-  cache: true,
-  entry: pathutil.resource('app') + '/app.js',
-  output: {
-    path: pathutil.resource('build'),
-    publicPath: '/static/build/',
-    filename: 'bundle.js'
-  },
-  resolve: {
+  cache: true
+, entry: pathutil.resource('app/app.js')
+, output: {
+    path: pathutil.resource('build')
+  , publicPath: '/static/build/'
+  , filename: 'bundle.js'
+  }
+, resolve: {
     modulesDirectories: [
-      pathutil.resource('bower_components'),
-        pathutil.resource('app') + '/components',
-      'web_modules',
-      './node_modules'
-    ],
-    alias: {
-      'angular-bootstrap': 'angular-bootstrap/ui-bootstrap-tpls',
-      'localforage': 'localforage/dist/localforage.js',
-      'socket.io': 'socket.io-client/dist/socket.io',
-      'oboe': 'oboe/dist/oboe-browser',
-      'ng-file-upload': 'ng-file-upload/angular-file-upload'
-    }
-  },
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: 'style!css' },
-      { test: /\.jpg$/, loader: "url-loader?limit=5000&mimetype=image/jpeg" },
-      { test: /\.png$/, loader: "url-loader?limit=5000&mimetype=image/png" },
-      { test: /\.gif$/, loader: "url-loader?limit=5000&mimetype=image/gif" },
-      { test: /\.svg$/
-      , loader: "url-loader?limit=5000&mimetype=image/svg+xml" },
-      { test: /\.woff$/
-      , loader: "url-loader?limit=5000&mimetype=application/font-woff" },
-      { test: /\.otf$/
-      , loader: "url-loader?limit=5000&mimetype=application/font-woff" },
-      { test: /\.ttf$/
-      , loader: "url-loader?limit=5000&mimetype=application/font-woff" },
-      { test: /\.eot$/
-      , loader: "url-loader?limit=5000&mimetype=vnd.ms-fontobject" },
-      { test: /\.jade/
-      , loader: 'template-html-loader' },
-      { test: /\.html/, loader: 'html-loader' },
-      { test: /angular\.js/, loader: 'exports?angular'},
-      { test: /angular-route\.js/, loader: 'imports?angular=angular'},
-      { test: /oboe-browser\.js/, loader: 'imports?define=>false!exports?oboe'},
-      { test: /localforage\.js/, loader: 'script'},
-      { test: /ui-bootstrap-tpls\.js/, loader: 'script'},
-      { test: /dialogs\.js/, loader: 'script'}
-    ],
-    noParse: [
-      //  pathutil.resource('bower_components')
+      pathutil.resource('bower_components')
+    , pathutil.resource('app/components')
+    , 'web_modules'
+    , './node_modules'
     ]
-  },
-  plugins: [
+  , alias: {
+      'angular-bootstrap': 'angular-bootstrap/ui-bootstrap-tpls'
+    , 'localforage': 'localforage/dist/localforage.js'
+    , 'socket.io': 'socket.io-client/dist/socket.io'
+    , 'oboe': 'oboe/dist/oboe-browser'
+    , 'ng-file-upload': 'ng-file-upload/angular-file-upload'
+    }
+  }
+, module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' }
+    , { test: /\.jpg$/, loader: "url-loader?limit=5000&mimetype=image/jpeg" }
+    , { test: /\.png$/, loader: "url-loader?limit=5000&mimetype=image/png" }
+    , { test: /\.gif$/, loader: "url-loader?limit=5000&mimetype=image/gif" }
+    , { test: /\.svg$/
+      , loader: "url-loader?limit=5000&mimetype=image/svg+xml" }
+    , { test: /\.woff$/
+      , loader: "url-loader?limit=5000&mimetype=application/font-woff" }
+    , { test: /\.otf$/
+      , loader: "url-loader?limit=5000&mimetype=application/font-woff" }
+    , { test: /\.ttf$/
+      , loader: "url-loader?limit=5000&mimetype=application/font-woff" }
+    , { test: /\.eot$/
+      , loader: "url-loader?limit=5000&mimetype=vnd.ms-fontobject" }
+    , { test: /\.jade/
+      , loader: 'template-html-loader' }
+    , { test: /\.html/, loader: 'html-loader' }
+    , { test: /angular\.js/, loader: 'exports?angular'}
+    , { test: /angular-route\.js/, loader: 'imports?angular=angular'}
+    , { test: /oboe-browser\.js/, loader: 'imports?define=>false!exports?oboe'}
+    , { test: /localforage\.js/, loader: 'script'}
+    , { test: /ui-bootstrap-tpls\.js/, loader: 'script'}
+    , { test: /dialogs\.js/, loader: 'script'}
+    ]
+  , noParse: [
+      // pathutil.resource('bower_components')
+    ]
+  }
+, plugins: [
     new webpack.ResolverPlugin(
       new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(
         'bower.json'
       , ['main']
       )
-    ),
-    new webpack.ResolverPlugin(
+    )
+  , new webpack.ResolverPlugin(
       new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(
         '.bower.json'
       , ['main']

6638da01a6adbc3ad27c1b8eca1a05249e956026
Gunther Brunner <gunta85@gmail.com>
2014-03-28 19:48:34 +0900

    - Fixed ugly css bug.

--- res/app/layout/index.js
+++ res/app/layout/index.js
@@ -3,16 +3,15 @@ require('se7en-bootstrap-3/build/stylesheets/se7en-font.css')
 require('se7en-bootstrap-3/build/stylesheets/style.css')
 require('se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')
 
-
 require('fa-borderlayout')
 require('fa-borderlayout/build-0.3.1/borderLayout.css')
 require('fa-borderlayout/build-0.3.1/stf-style.css')
 
-
 require('./cursor.css')
 require('./stf-se7en.css')
-require('./stf-styles.css')
 require('./small.css')
+require('./stf-styles.css')
+
 
 module.exports = angular.module('layout', [
   'fa.directive.borderLayout',

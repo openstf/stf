8cbc89b96f86b0dfd1fbddcd06392fcb19e0c77e
Gunther Brunner <gunta85@gmail.com>
2014-04-15 15:37:44 +0900

    Port forwarding now adds rows in real time.

--- res/app/control-panes/advanced/port-forwarding/port-forwarding-controller.js
+++ res/app/control-panes/advanced/port-forwarding/port-forwarding-controller.js
@@ -1,21 +1,54 @@
+var _ = require('lodash')
+
 module.exports = function PortForwardingCtrl($scope, ngTableParams) {
 
   $scope.portSets = [
     {
-      targetHost: '',
-      targetPort: '',
-      devicePort: ''
+      targetHost: 'local',
+      targetPort: '23',
+      devicePort: '444'
+    },
+    {
+      targetHost: '111',
+      targetPort: '222',
+      devicePort: '333'
     }
   ]
 
+  function portFieldsAreEmpty(ports) {
+    return (_.isEmpty(ports.targetHost) && _.isEmpty(ports.targetPort) && _.isEmpty(ports.devicePort))
+  }
+
+  // Adds a new row whenever necessary
+  $scope.$watch('portSets', function (newValue, oldValue) {
+    // Remove all empty sets from the middle
+    _.remove(newValue, function (ports, index) {
+      // Skip last and remove empty fields
+      return !!(newValue.length !== index + 1 && portFieldsAreEmpty(ports))
+    })
+
+    var last = _.last(newValue)
+    if (!portFieldsAreEmpty(last)) {
+      var empty = {
+        targetLocal: null,
+        targetHost: null,
+        targetPort: null,
+        devicePort: null,
+      }
+      $scope.portSets.push(empty)
+    }
+  }, true)
+
   $scope.portsTable = new ngTableParams({
+    page: 1,
+    count: 5
   }, {
     counts: [],
     total: 1,
     getData: function ($defer, params) {
-      var data = $scope.portSets
 
-      $defer.resolve(data);
+
+      $defer.resolve($scope.portSets);
     }
   })
 }

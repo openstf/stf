fb3892ab07d332197f12cffb82b7fbd90a8ea7d4
Simo Kinnunen <simo@shoqolate.com>
2014-04-15 18:02:48 +0900

    Keep forwards list up to date on client side.

--- res/app/components/stf/user/user-service.js
+++ res/app/components/stf/user/user-service.js
@@ -1,4 +1,4 @@
-module.exports = function UserServiceFactory($http) {
+module.exports = function UserServiceFactory($http, $rootScope, socket) {
   var userService = {}
 
   userService.user = (function () {
@@ -7,6 +7,24 @@ module.exports = function UserServiceFactory($http) {
       return userPromise
     }
   })()
+
+  socket.on('forward.create', function(data) {
+    userService.user().then(function(user) {
+      $rootScope.$apply(function() {
+        user.forwards.push(data)
+      })
+    })
+  })
+
+  socket.on('forward.remove', function(data) {
+    userService.user().then(function(user) {
+      $rootScope.$apply(function() {
+        user.forwards = user.forwards.filter(function(forward) {
+          return forward.devicePort !== data.devicePort
+        })
+      })
+    })
+  })
 
   return userService
 }

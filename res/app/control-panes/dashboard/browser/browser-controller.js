f8315962901a39a94e310cf934fc464f1a114cf3
Gunther Brunner <gunta85@gmail.com>
2014-04-22 16:51:16 +0900

    - Adding Browser selection. - Adding URL history and autocomplete.

--- res/app/control-panes/dashboard/browser/browser-controller.js
+++ res/app/control-panes/dashboard/browser/browser-controller.js
@@ -1,3 +1,26 @@
-module.exports = function BrowserCtrl($scope) {
-    
+var _ = require('lodash')
+
+module.exports = function BrowserCtrl($scope, $rootScope) {
+
+  function setCurrentBrowser(device) {
+    if (device && device.browser && device.browser.apps) {
+      var currentBrowser = {}
+      var selectedBrowser = _.first(device.browser.apps, 'selected')
+      if (!_.isEmpty(selectedBrowser)) {
+        currentBrowser = selectedBrowser[0]
+      } else {
+        currentBrowser = _.first(device.browser.apps)
+      }
+      $rootScope.browser = currentBrowser
+    }
+  }
+
+  setCurrentBrowser($scope.device)
+
+  $scope.$watch('device', function (newValue, oldValue) {
+    if (newValue !== oldValue) {
+      setCurrentBrowser(newValue)
+    }
+  })
+
 }
--- res/app/control-panes/dashboard/navigation/navigation-controller.js
+++ res/app/control-panes/dashboard/navigation/navigation-controller.js
@@ -1,10 +1,46 @@
-module.exports = function NavigationCtrl($scope) {
-  $scope.activeBrowser = null
+var _ = require('lodash')
 
-  $scope.openURL = function() {
+module.exports = function NavigationCtrl($scope) {
+
+  $scope.urlHistory = []
+
+  function addToHistory() {
+    const HISTORY_LIMIT = 20
+
+    var history = $scope.urlHistory
+    history.unshift($scope.textURL)
+    if (history.length > HISTORY_LIMIT) {
+      history.pop()
+    }
+    $scope.urlHistory = _.uniq(history)
+  }
+
+  function addHttp() {
+    if ($scope.textURL.indexOf(':') === -1) {
+      $scope.textURL = 'http://' + $scope.textURL
+    }
+  }
+
+  $scope.openURL = function () {
+    addHttp()
+    addToHistory()
+
     return $scope.control.openBrowser(
-      $scope.textURL
-    , $scope.activeBrowser
+      $scope.textURL,
+      $scope.browser
     )
+  }
+
+  $scope.clearHistory = function () {
+    $scope.urlHistory = []
+  }
+
+  $scope.hasHistory = function () {
+    return $scope.urlHistory.length > 0
+  }
+
+  $scope.insertURL = function ($url) {
+    $scope.textURL = $url
+    $scope.openURL()
   }
 }

47cd0313a13df0324cabf8fabd6b459494b28968
Gunther Brunner <gunta85@gmail.com>
2014-04-22 21:11:46 +0900

    - Moved browser selection to Navigation.

--- res/app/control-panes/dashboard/browser/browser-controller.js
+++ res/app/control-panes/dashboard/browser/browser-controller.js
@@ -2,32 +2,6 @@ var _ = require('lodash')
 
 module.exports = function BrowserCtrl($scope, $rootScope) {
 
-  function setCurrentBrowser(device) {
-    if (device && device.browser && device.browser.apps) {
-      var currentBrowser = {}
-      if (device.browser.selected) {
-        var selectedBrowser = _.first(device.browser.apps, 'selected')
-        if (!_.isEmpty(selectedBrowser)) {
-          currentBrowser = selectedBrowser[0]
-        }
-      } else {
-        currentBrowser = _.first(device.browser.apps)
-      }
-      $rootScope.browser = currentBrowser
-    }
-  }
 
-  setCurrentBrowser($scope.device)
-
-  $scope.$watch('device', function (newValue, oldValue) {
-    if (newValue !== oldValue) {
-      setCurrentBrowser(newValue)
-    }
-  })
-
-  $scope.clearSettings = function () {
-    var browser = $scope.browser
-    $scope.control.clearBrowser(browser)
-  }
 
 }
--- res/app/control-panes/dashboard/navigation/navigation-controller.js
+++ res/app/control-panes/dashboard/navigation/navigation-controller.js
@@ -1,6 +1,6 @@
 var _ = require('lodash')
 
-module.exports = function NavigationCtrl($scope) {
+module.exports = function NavigationCtrl($scope, $rootScope) {
 
   $scope.urlHistory = []
 
@@ -42,5 +42,34 @@ module.exports = function NavigationCtrl($scope) {
   $scope.insertURL = function ($url) {
     $scope.textURL = $url
     $scope.openURL()
+  }
+
+
+  function setCurrentBrowser(device) {
+    if (device && device.browser && device.browser.apps) {
+      var currentBrowser = {}
+      if (device.browser.selected) {
+        var selectedBrowser = _.first(device.browser.apps, 'selected')
+        if (!_.isEmpty(selectedBrowser)) {
+          currentBrowser = selectedBrowser[0]
+        }
+      } else {
+        currentBrowser = _.first(device.browser.apps)
+      }
+      $rootScope.browser = currentBrowser
+    }
+  }
+
+  setCurrentBrowser($scope.device)
+
+  $scope.$watch('device', function (newValue, oldValue) {
+    if (newValue !== oldValue) {
+      setCurrentBrowser(newValue)
+    }
+  })
+
+  $scope.clearSettings = function () {
+    var browser = $scope.browser
+    $scope.control.clearBrowser(browser)
   }
 }

254b2dc7a3ba5ff2bf607166a8edb5e8a2c18756
Simo Kinnunen <simo@shoqolate.com>
2014-05-20 11:34:39 +0900

    Add screenshots to UI. Still need to implement resizing.

--- res/app/control-panes/screenshots/screenshots-controller.js
+++ res/app/control-panes/screenshots/screenshots-controller.js
@@ -1,3 +1,31 @@
-module.exports = function ScreenshotsCtrl($scope) {
-    
+module.exports = function ScreenshotsCtrl($scope, SettingsService) {
+  $scope.screenshots = []
+  $scope.shotSize = 'original'
+
+  $scope.clear = function () {
+    $scope.screenshots = []
+  }
+
+  SettingsService.bind($scope, {
+    key: 'shotSize'
+  , storeName: 'ScreenShots.shotSize'
+  })
+
+  $scope.shotSizeUrlParameter = function () {
+    var sizes = {
+      'small': '?crop=100x',
+      'medium': '?crop=320x',
+      'large': '?crop=450x',
+      'original': ''
+    }
+    return sizes[$scope.shotSize]
+  }
+
+  $scope.takeScreenShot = function () {
+    $scope.control.screenshot().then(function(result) {
+      $scope.$apply(function() {
+        $scope.screenshots.push(result)
+      })
+    })
+  }
 }

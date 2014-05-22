21d4480f77e645937424d3d53f8dfb71a0afb442
Gunther Brunner <gunta85@gmail.com>
2014-05-22 23:23:16 +0900

    Added a new font Lato 2 (made a bower package for it). Trying to solve the btn:focus black problem.

--- res/app/layout/index.js
+++ res/app/layout/index.js
@@ -1,7 +1,15 @@
-require('se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
+//require('se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
+require('bootstrap/dist/css/bootstrap.css')
+
+
+
 require('se7en-bootstrap-3/build/stylesheets/se7en-font.css')
 require('se7en-bootstrap-3/build/stylesheets/style.css')
-require('se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')
+
+require('font-awesome-bower/css/font-awesome.css')
+
+require('font-lato-2-subset')
+//require('se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')
 
 require('angular-bootstrap')
 

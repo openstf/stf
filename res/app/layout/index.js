17e7b94d68477418229076c73c740ecf1b5b3c51
Gunther Brunner <gunta85@gmail.com>
2014-05-23 17:19:35 +0900

    Bootstrap forked.

--- res/app/layout/index.js
+++ res/app/layout/index.js
@@ -1,7 +1,5 @@
-//require('se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
-require('bootstrap/dist/css/bootstrap.css')
-
-
+require('se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
+//require('bootstrap/dist/css/bootstrap.css')
 
 require('se7en-bootstrap-3/build/stylesheets/se7en-font.css')
 require('se7en-bootstrap-3/build/stylesheets/style.css')

e5018234d46f7f880bbee668c9332818ba3739f6
Gunther Brunner <gunta85@gmail.com>
2014-05-29 17:11:14 +0900

    Added filter translate for websocket errors.

--- res/app/components/stf/socket/socket-state/socket-state-directive.js
+++ res/app/components/stf/socket/socket-state/socket-state-directive.js
@@ -1,4 +1,4 @@
-module.exports = function SocketStateDirectiveFactory(socket, growl, gettext) {
+module.exports = function SocketStateDirectiveFactory(socket, growl, gettext, $filter) {
   return {
     restrict: 'EA',
     template: require('./socket-state.jade'),
@@ -64,30 +64,30 @@ module.exports = function SocketStateDirectiveFactory(socket, growl, gettext) {
       scope.$watch('socketState', function (newValue, oldValue) {
         if (newValue) {
           if (newValue === 'connecting' && oldValue) {
-            growl.info('<h4>WebSocket</h4>' + gettext('Connecting...'), {ttl: 1000})
+            growl.info('<h4>WebSocket</h4>' + $filter('translate')(gettext('Connecting...')), {ttl: 1000})
           } else if (newValue === 'connect' && oldValue === 'connecting') {
             if (hasFailedOnce) {
-              growl.success('<h4>WebSocket</h4>' + gettext('Connected successfully.') + '<refresh-page></refresh-page>', {ttl: 2000})
+              growl.success('<h4>WebSocket</h4>' + $filter('translate')(gettext('Connected successfully.')) + '<refresh-page></refresh-page>', {ttl: 2000})
             }
           } else {
             switch (newValue) {
               case 'disconnect':
-                growl.error('<h4>WebSocket</h4>' + gettext('Disconnected.<br />Socket connection was lost, try again reloading the page.'), {ttl: -1})
+                growl.error('<h4>WebSocket</h4>' +  $filter('translate')(gettext('Disconnected.<br />Socket connection was lost, try again reloading the page.')), {ttl: -1})
                 break;
               case 'connect_failed':
-                growl.error('<h4>WebSocket</h4>' + gettext('Error while connecting.'), {ttl: -1})
+                growl.error('<h4>WebSocket</h4>' + $filter('translate')(gettext('Error while connecting.')), {ttl: -1})
                 break;
               case 'error':
-                growl.error('<h4>WebSocket</h4>' + gettext('Error.'), {ttl: -1})
+                growl.error('<h4>WebSocket</h4>' + $filter('translate')(gettext('Error.'), {ttl: -1}))
                 break;
               case 'reconnect_failed':
-                growl.error('<h4>WebSocket</h4>' + gettext('Error while reconnecting.'), {ttl: -1})
+                growl.error('<h4>WebSocket</h4>' + $filter('translate')(gettext('Error while reconnecting.')), {ttl: -1})
                 break;
               case 'reconnect':
-                growl.success('<h4>WebSocket</h4>' + gettext('Reconnected successfully.'), {ttl: -1})
+                growl.success('<h4>WebSocket</h4>' + $filter('translate')(gettext('Reconnected successfully.')), {ttl: -1})
                 break;
               case 'reconnecting':
-                growl.error('<h4>WebSocket</h4>' + gettext('Reconnecting...') + '<refresh-page></refresh-page>', {ttl: -1})
+                growl.error('<h4>WebSocket</h4>' + $filter('translate')(gettext('Reconnecting...')) + '<refresh-page></refresh-page>', {ttl: -1})
                 break;
             }
           }

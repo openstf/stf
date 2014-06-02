9239ee57b44cbda9689d778e0d79d42dc9520d08
Gunther Brunner <gunta85@gmail.com>
2014-06-02 17:49:32 +0900

    Adding protractor.

---
+++ protractor.conf.js
@@ -0,0 +1,5 @@
+// Reference: https://github.com/angular/protractor/blob/master/referenceConf.js
+
+exports.config = {
+  specs: ['res/test/e2e/*spec.js']
+}
---
+++ res/test/e2e/stfAppE2E-spec.js
@@ -0,0 +1,11 @@
+describe('stf app', function () {
+  describe('App', function () {
+    it('should show a list of devices', function () {
+
+      // TODO: let's get rid off the login first
+      browser.get('http://localhost:7100/')
+
+      expect(true).toEqual(true)
+    })
+  })
+})

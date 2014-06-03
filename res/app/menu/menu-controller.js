bad0ca9cc7d797eb31d61ba11b6c6c8d5f7132cb
Gunther Brunner <gunta85@gmail.com>
2014-06-03 19:35:02 +0900

    Show only Web/Native when controlling.

--- res/app/menu/menu-controller.js
+++ res/app/menu/menu-controller.js
@@ -1,7 +1,11 @@
-module.exports = function MenuCtrl($scope, $rootScope, SettingsService) {
+module.exports = function MenuCtrl($scope, $rootScope, SettingsService, $location) {
   $rootScope.platform = 'native'
   SettingsService.bind($rootScope, {
     key: 'platform',
     storeName: 'Platform'
+  })
+
+  $scope.$on('$routeChangeSuccess', function () {
+    $scope.isControlRoute = $location.path().search('/control') !== -1
   })
 }

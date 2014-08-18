74c30a70b91d97c2d7397ebcba982eb25ff47892
Gunther Brunner <gunta85@gmail.com>
2014-08-18 18:56:58 +0900

    Added safety 2nd timeout to startRemoteConnect.

--- res/app/control-panes/dashboard/remote-debug/remote-debug-controller.js
+++ res/app/control-panes/dashboard/remote-debug/remote-debug-controller.js
@@ -1,17 +1,24 @@
 module.exports = function RemoteDebugCtrl($scope, $timeout) {
   function startRemoteConnect() {
-    $scope.control.startRemoteConnect().then(function (result) {
-      var url = result.lastData
-      $scope.$apply(function () {
-        $scope.debugCommand = 'adb connect ' + url
+    if ($scope.control) {
+      $scope.control.startRemoteConnect().then(function (result) {
+        var url = result.lastData
+        $scope.$apply(function () {
+          $scope.debugCommand = 'adb connect ' + url
+        })
       })
-    })
+
+      return true
+    }
+    return false
   }
 
   // TODO: Remove timeout and fix control initialization
-  if ($scope.control) {
-    startRemoteConnect()
-  } else {
-    $timeout(startRemoteConnect, 200)
+  if (!startRemoteConnect()) {
+    $timeout(function () {
+      if (!startRemoteConnect()) {
+        $timeout(startRemoteConnect, 1000)
+      }
+    }, 200)
   }
 }

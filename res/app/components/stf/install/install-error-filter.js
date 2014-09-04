fdfcdf913d05331543865590fee842ded3cc23a5
Simo Kinnunen <simo@shoqolate.com>
2014-09-04 16:51:43 +0900

    Move install response codes to the filter.

--- res/app/components/stf/install/install-error-filter.js
+++ res/app/components/stf/install/install-error-filter.js
@@ -1,12 +1,10 @@
 // From here: https://github.com/android/platform_frameworks_base/blob/
 // master/core/java/android/content/pm/PackageManager.java#L371
 
+var responses = require('./response-codes.json')
+
 module.exports = function installErrorFilter(gettext) {
   return function (text) {
-    return {
-      INSTALL_FAILED_OLDER_SDK: gettext('New package failed because the ' +
-      'current SDK version is older than that required by the package')
-      // TODO: do the rest
-    }[text] || text
+    return gettext(responses[text] || text)
   }
 }

5a52b57e741bea44ee1eaa5092ea08352f39ef2d
Gunther Brunner <gunta85@gmail.com>
2014-09-11 20:32:32 +0900

    Add some translation.

--- res/app/components/stf/language/language-service.js
+++ res/app/components/stf/language/language-service.js
@@ -1,46 +1,43 @@
 var supportedLanguages = require('./../../../../common/lang/langs.json')
 
-module.exports = function LanguageServiceFactory(
-  SettingsService
-, gettextCatalog
-) {
-  // TODO: make this LanguageProvider so it can be used on config
+module.exports =
+  function LanguageServiceFactory(SettingsService, gettextCatalog) {
+    // TODO: make this LanguageProvider so it can be used on config
 
-  var LanguageService = {}
+    var LanguageService = {}
 
-  function detectLanguage() {
-    return (navigator.language || navigator.userLanguage || 'en-US')
-      .substring(0, 2)
-  }
-
-  function isSupported(lang) {
-    return !!supportedLanguages[lang]
-  }
-
-  function onlySupported(lang, defaultValue) {
-    return isSupported(lang) ? lang : defaultValue
-  }
-
-  LanguageService.supportedLanguages = supportedLanguages
-  LanguageService.defaultLanguage = 'en'
-  LanguageService.detectedLanguage =
-    onlySupported(detectLanguage(), LanguageService.defaultLanguage)
-
-  SettingsService.sync(
-    LanguageService
-  , {
-      target: 'selectedLanguage'
-    , source: 'selectedLanguage'
-    , defaultValue: LanguageService.detectedLanguage
+    function detectLanguage() {
+      return (navigator.language || navigator.userLanguage || 'en-US')
+        .substring(0, 2)
     }
-  , updateLanguage
-  )
 
-  function updateLanguage() {
-    gettextCatalog.setCurrentLanguage(LanguageService.selectedLanguage)
+    function isSupported(lang) {
+      return !!supportedLanguages[lang]
+    }
+
+    function onlySupported(lang, defaultValue) {
+      return isSupported(lang) ? lang : defaultValue
+    }
+
+    LanguageService.settingKey = 'selectedLanguage'
+    LanguageService.supportedLanguages = supportedLanguages
+    LanguageService.defaultLanguage = 'en'
+    LanguageService.detectedLanguage =
+      onlySupported(detectLanguage(), LanguageService.defaultLanguage)
+
+    SettingsService.sync(
+      LanguageService, {
+        target: LanguageService.settingKey,
+        source: LanguageService.settingKey,
+        defaultValue: LanguageService.detectedLanguage
+      }, updateLanguage
+    )
+
+    function updateLanguage() {
+      gettextCatalog.setCurrentLanguage(LanguageService.selectedLanguage)
+    }
+
+    LanguageService.updateLanguage = updateLanguage
+
+    return LanguageService
   }
-
-  LanguageService.updateLanguage = updateLanguage
-
-  return LanguageService
-}

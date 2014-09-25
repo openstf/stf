2f1908b7182f7e55e07631fd303dfb58133811dc
Gunther Brunner <gunta85@gmail.com>
2014-09-25 18:33:20 +0900

    Add save to disk ScreenShot.

--- res/app/components/stf/device-context-menu/device-context-menu-directive.js
+++ res/app/components/stf/device-context-menu/device-context-menu-directive.js
@@ -15,6 +15,35 @@ module.exports = function deviceContextMenuDirective($window) {
         $window.close()
       }
 
+      function saveToDisk(fileURL, fileName) {
+        // for non-IE
+        if (!window.ActiveXObject) {
+          var save = document.createElement('a')
+          save.href = fileURL
+          save.target = '_blank'
+          save.download = fileName || 'unknown'
+
+          var event = document.createEvent('Event')
+          event.initEvent('click', true, true)
+          save.dispatchEvent(event)
+          (window.URL || window.webkitURL).revokeObjectURL(save.href)
+        }
+
+        // for IE
+        else if (!!window.ActiveXObject && document.execCommand) {
+          var _window = window.open(fileURL, '_blank')
+          _window.document.close()
+          _window.document.execCommand('SaveAs', true, fileName || fileURL)
+          _window.close()
+        }
+      }
+
+      scope.saveScreenShot = function () {
+        scope.control.screenshot().then(function (result) {
+          saveToDisk(result.body.href, result.body.name)
+        })
+      }
+
     }
   }
 }

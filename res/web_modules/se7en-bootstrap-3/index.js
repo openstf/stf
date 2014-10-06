1bedbad65071e6b27dbddbd138eba279a1b651fe
Gunther Brunner <gunta85@gmail.com>
2014-10-06 19:05:03 +0900

    Replaced 'font-awesome-bower' -> 'components-font-awesome' because it's more up-to-date.

--- res/web_modules/se7en-bootstrap-3/index.js
+++ res/web_modules/se7en-bootstrap-3/index.js
@@ -4,7 +4,6 @@ require('se7en-bootstrap-3/build/stylesheets/bootstrap.min.css')
 require('se7en-bootstrap-3/build/stylesheets/se7en-font.css')
 require('se7en-bootstrap-3/build/stylesheets/style.css')
 
-require('font-awesome-bower/css/font-awesome.css')
+require('components-font-awesome')
 
 require('font-lato-2-subset')
-//require('se7en-bootstrap-3/build/stylesheets/font-awesome.min.css')

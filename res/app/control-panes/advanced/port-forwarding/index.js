15755334d555677d84df2eef7ced85ab61a63f26
Gunther Brunner <gunta85@gmail.com>
2014-10-14 20:42:55 +0900

    At least some UI for the new Port Forwarding.

--- res/app/control-panes/advanced/port-forwarding/index.js
+++ res/app/control-panes/advanced/port-forwarding/index.js
@@ -3,8 +3,13 @@ require('./port-forwarding.css')
 module.exports = angular.module('stf.port-forwarding', [
   require('stf/common-ui/table').name,
   require('stf/settings').name,
-  require('gettext').name
+  require('gettext').name,
+  require('angular-xeditable').name
 ])
+  .run(function (editableOptions) {
+    editableOptions.theme = 'bs3'
+
+  })
   .run(["$templateCache", function ($templateCache) {
     $templateCache.put(
       'control-panes/advanced/port-forwarding/port-forwarding.jade',
--- res/app/control-panes/advanced/port-forwarding/port-forwarding-controller.js
+++ res/app/control-panes/advanced/port-forwarding/port-forwarding-controller.js
@@ -1,34 +1,64 @@
-module.exports = function PortForwardingCtrl(
-  $scope
-, SettingsService
-) {
-  $scope.reversePortForwards = [
-    {
-      targetHost: 'localhost'
+var _ = require('lodash')
+
+module.exports = function PortForwardingCtrl($scope
+  , SettingsService) {
+
+  var defaultPortForwards = {
+    targetHost: 'localhost'
     , targetPort: 8080
     , devicePort: 8080
     , enabled: false
-    }
+  }
+
+  $scope.reversePortForwards = [
+    defaultPortForwards
   ]
 
+  // TODO: tracking by devicePort needs a bit of care
   SettingsService.bind($scope, {
     target: 'reversePortForwards'
-  , source: 'reversePortForwards'
+    , source: 'reversePortForwards'
   })
 
-  $scope.enableForward = function(forward) {
+  $scope.enableForward = function (forward) {
     forward.processing = true
     return $scope.control.createForward(forward)
-      .finally(function() {
+      .finally(function () {
         forward.processing = false
       })
   }
 
-  $scope.disableForward = function(forward) {
+  $scope.disableForward = function (forward) {
     forward.processing = true
     return $scope.control.removeForward(forward)
-      .finally(function() {
+      .finally(function () {
         forward.processing = false
       })
+  }
+
+  function ensureUniqueDevicePorts() {
+    $scope.reversePortForwards =
+      _.uniq($scope.reversePortForwards, 'devicePort')
+  }
+
+  $scope.addRow = function () {
+    // TODO: tracking by devicePort needs a bit of care
+    $scope.reversePortForwards.push(defaultPortForwards)
+    ensureUniqueDevicePorts()
+  }
+
+  $scope.removeRow = function (forward) {
+    $scope.disableForward(forward)
+    $scope.reversePortForwards.splice(
+      $scope.reversePortForwards.indexOf(forward), 1)
+  }
+
+  $scope.showApply = function () {
+    $scope.isShowApply = true
+  }
+
+  $scope.applyTable = function () {
+    console.log('TODO: Sync back the changes')
+    $scope.isShowApply = false
   }
 }

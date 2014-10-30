fc025d16aa4c2642f92783d583610f99cc3006cc
Simo Kinnunen <simo@shoqolate.com>
2014-10-30 21:32:18 +0900

    Formatting.

--- res/app/components/stf/socket/socket-service.js
+++ res/app/components/stf/socket/socket-service.js
@@ -1,42 +1,45 @@
 var io = require('socket.io')
 
-module.exports =
-  function SocketFactory($rootScope, VersionUpdateService, AppState) {
-    var websocketUrl = AppState.config.websocketUrl || ''
+module.exports = function SocketFactory(
+  $rootScope
+, VersionUpdateService
+, AppState
+) {
+  var websocketUrl = AppState.config.websocketUrl || ''
 
-    var socket = io(websocketUrl, {
-      reconnection: false, transports: ['websocket']
+  var socket = io(websocketUrl, {
+    reconnection: false, transports: ['websocket']
+  })
+
+  socket.scoped = function ($scope) {
+    var listeners = []
+
+    $scope.$on('$destroy', function () {
+      listeners.forEach(function (listener) {
+        socket.removeListener(listener.event, listener.handler)
+      })
     })
 
-    socket.scoped = function ($scope) {
-      var listeners = []
-
-      $scope.$on('$destroy', function () {
-        listeners.forEach(function (listener) {
-          socket.removeListener(listener.event, listener.handler)
+    return {
+      on: function (event, handler) {
+        listeners.push({
+          event: event, handler: handler
         })
-      })
-
-      return {
-        on: function (event, handler) {
-          listeners.push({
-            event: event, handler: handler
-          })
-          socket.on(event, handler)
-          return this
-        }
+        socket.on(event, handler)
+        return this
       }
     }
-
-    socket.on('outdated', function () {
-      VersionUpdateService.open()
-    })
-
-    socket.on('socket.ip', function (ip) {
-      $rootScope.$apply(function () {
-        socket.ip = ip
-      })
-    })
-
-    return socket
   }
+
+  socket.on('outdated', function () {
+    VersionUpdateService.open()
+  })
+
+  socket.on('socket.ip', function (ip) {
+    $rootScope.$apply(function () {
+      socket.ip = ip
+    })
+  })
+
+  return socket
+}

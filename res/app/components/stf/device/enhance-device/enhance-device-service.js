cf73b4c95be836120eb5c26df903465853e3d2bf
Simo Kinnunen <simo@shoqolate.com>
2015-07-01 20:57:33 +0900

    Add filter-branched files back.

---
+++ res/app/components/stf/device/enhance-device/enhance-device-service.js
@@ -0,0 +1,93 @@
+module.exports = function EnhanceDeviceServiceFactory($filter, AppState) {
+  var service = {}
+
+  function setState(data) {
+    // For convenience, formulate an aggregate state property that covers
+    // every possible state.
+    data.state = 'absent'
+    if (data.present) {
+      data.state = 'present'
+      switch (data.status) {
+        case 1:
+          data.state = 'offline'
+          break
+        case 2:
+          data.state = 'unauthorized'
+          break
+        case 3:
+          data.state = 'preparing'
+          if (data.ready) {
+            data.state = 'ready'
+            if (data.using) {
+              data.state = 'using'
+            }
+            else {
+              if (data.owner) {
+                data.state = 'busy'
+              }
+              else {
+                data.state = 'available'
+              }
+            }
+          }
+          break
+      }
+    }
+  }
+
+  function enhanceDevice(device) {
+    device.enhancedName = device.name || device.model || device.serial || 'Unknown'
+    device.enhancedModel = device.model || 'Unknown'
+    device.enhancedImage120 = '/static/app/devices/icon/x120/' + (device.image || '_default.jpg')
+    device.enhancedImage24 = '/static/app/devices/icon/x24/' + (device.image || '_default.jpg')
+    device.enhancedStateAction = $filter('statusNameAction')(device.state)
+    device.enhancedStatePassive = $filter('statusNamePassive')(device.state)
+  }
+
+  function enhanceDeviceDetails(device) {
+    if (device.battery) {
+      device.enhancedBatteryPercentage = (device.battery.level / device.battery.scale * 100) + '%'
+      device.enhancedBatteryHealth = $filter('batteryHealth')(device.battery.health)
+      device.enhancedBatterySource = $filter('batterySource')(device.battery.source)
+      device.enhancedBatteryStatus = $filter('batteryStatus')(device.battery.status)
+      device.enhancedBatteryTemp = device.battery.temp + '°C'
+    }
+
+    if (device.owner) {
+      device.enhancedUserProfileUrl = enhanceUserProfileUrl(device.owner.email)
+      device.enhancedUserName = device.owner.name || "No name"
+    }
+  }
+
+  function enhanceUserProfileUrl(email) {
+    var url
+    var userProfileUrl = (function () {
+      if (AppState && AppState.config && AppState.config.userProfileUrl) {
+        return AppState.config.userProfileUrl
+      }
+      return null
+    })()
+
+    if (userProfileUrl) {
+      // Using RFC 6570 URI Template specification
+      if (userProfileUrl && email) {
+        url = userProfileUrl.indexOf('{user}') !== -1 ?
+          userProfileUrl.replace('{user}', email) :
+          userProfileUrl + email;
+      }
+    } else if (email.indexOf('@') !== -1) {
+      url = 'mailto:' + email
+    } else {
+      url = '/!#/user/' + email
+    }
+    return url
+  }
+
+  service.enhance = function (device) {
+    setState(device)
+    enhanceDevice(device)
+    enhanceDeviceDetails(device)
+  }
+
+  return service
+}
---
+++ res/app/device-list/device-list-controller.js
@@ -0,0 +1,192 @@
+var QueryParser = require('./util/query-parser')
+
+module.exports = function DeviceListCtrl(
+  $scope
+, DeviceService
+, DeviceColumnService
+, GroupService
+, ControlService
+, SettingsService
+, $location
+) {
+  $scope.tracker = DeviceService.trackAll($scope)
+  $scope.control = ControlService.create($scope.tracker.devices, '*ALL')
+
+  $scope.columnDefinitions = DeviceColumnService
+
+  var defaultColumns = [
+    {
+      name: 'state'
+    , selected: true
+    }
+  , {
+      name: 'model'
+    , selected: true
+    }
+  , {
+      name: 'name'
+    , selected: true
+    }
+  , {
+      name: 'serial'
+    , selected: false
+    }
+  , {
+      name: 'operator'
+    , selected: true
+    }
+  , {
+      name: 'releasedAt'
+    , selected: true
+    }
+  , {
+      name: 'version'
+    , selected: true
+    }
+  , {
+      name: 'network'
+    , selected: false
+    }
+  , {
+      name: 'display'
+    , selected: false
+    }
+  , {
+      name: 'manufacturer'
+    , selected: false
+    }
+  , {
+      name: 'sdk'
+    , selected: false
+    }
+  , {
+      name: 'abi'
+    , selected: false
+    }
+  , {
+      name: 'browser'
+    , selected: false
+    }
+  , {
+      name: 'phone'
+    , selected: false
+    }
+  , {
+      name: 'imei'
+    , selected: false
+    }
+  , {
+      name: 'iccid'
+    , selected: false
+    }
+  , {
+      name: 'batteryHealth'
+    , selected: false
+    }
+  , {
+      name: 'batterySource'
+    , selected: false
+    }
+  , {
+      name: 'batteryStatus'
+    , selected: false
+    }
+  , {
+      name: 'batteryLevel'
+    , selected: false
+    }
+  , {
+      name: 'batteryTemp'
+    , selected: false
+    }
+  , {
+      name: 'provider'
+    , selected: true
+    }
+  , {
+      name: 'notes'
+    , selected: true
+    }
+  , {
+      name: 'owner'
+    , selected: true
+    }
+  ]
+
+  $scope.columns = defaultColumns
+
+  SettingsService.bind($scope, {
+    target: 'columns'
+  , source: 'deviceListColumns'
+  })
+
+  var defaultSort = {
+    fixed: [
+      {
+        name: 'state'
+        , order: 'asc'
+      }
+    ]
+    , user: [
+      {
+        name: 'name'
+        , order: 'asc'
+      }
+    ]
+  }
+
+  $scope.sort = defaultSort
+
+  SettingsService.bind($scope, {
+    target: 'sort'
+  , source: 'deviceListSort'
+  })
+
+  $scope.filter = []
+
+  $scope.activeTabs = {
+    icons: true
+  , details: false
+  }
+
+  SettingsService.bind($scope, {
+    target: 'activeTabs'
+  , source: 'deviceListActiveTabs'
+  })
+
+  $scope.toggle = function (device) {
+    if (device.using) {
+      $scope.kick(device)
+    } else {
+      $location.path('/control/' + device.serial)
+    }
+  }
+
+  $scope.invite = function (device) {
+    return GroupService.invite(device).then(function () {
+      $scope.$digest()
+    })
+  }
+
+  $scope.applyFilter = function(query) {
+    $scope.filter = QueryParser.parse(query)
+  }
+
+  $scope.search = {
+    deviceFilter: '',
+    focusElement: false
+  }
+
+  $scope.focusSearch = function () {
+    if (!$scope.basicMode) {
+      $scope.search.focusElement = true
+    }
+  }
+
+  $scope.reset = function () {
+    $scope.search.deviceFilter = ''
+    $scope.filter = []
+    $scope.sort = defaultSort
+    $scope.columns = defaultColumns
+  }
+}

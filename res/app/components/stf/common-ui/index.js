bd493b210b4b1f3923c288f65f19ebc45f79d35b
Gunther Brunner <gunta85@gmail.com>
2015-08-31 15:58:32 +0900

    Cleanup code for stf components.

--- res/app/components/stf/common-ui/index.js
+++ res/app/components/stf/common-ui/index.js
@@ -8,7 +8,6 @@ module.exports = angular.module('stf/common-ui', [
   require('./notifications').name,
   require('./ng-enter').name,
   require('./tooltips').name,
-  //require('./tree').name,
   require('./modals').name,
   require('./include-cached').name,
   require('./text-focus-select').name,

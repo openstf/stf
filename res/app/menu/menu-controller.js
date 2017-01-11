c8a3b010a67d587fde75cfbd1de560b9620fe6b0
HellDima <d.ledevich@gmail.com>
2017-01-11 17:48:40 +0200

    add option of logout of STF system

--- res/app/menu/menu-controller.js
+++ res/app/menu/menu-controller.js
@@ -1,6 +1,5 @@
 module.exports = function MenuCtrl($scope, $rootScope, SettingsService,
-  $location, $http) {
-  console.log(global.app);
+  $location, AppState) {
   SettingsService.bind($scope, {
     target: 'lastUsedDevice'
   })
@@ -15,41 +14,20 @@ module.exports = function MenuCtrl($scope, $rootScope, SettingsService,
   })
 
   function resetPaths() {
-    // $scope = undefined
-    // $location = undefined
-    // AppState = undefined
-    console.log("start");
-    // var fs = require('fs')
-    // var express = require('express')
-
-    // var app = express()
-    // app.use(function(req, res, next) {
-    //   res.cookie("express.sid", "", { expires: new Date() })
-    //   next()
-    // })
-
-    // app.get('/', function(req, res) {
-    //   res.clearCookie("localhost")
-    // })
-
-    // AppState.config.websocketUrl = null
-    // $location.$$path = "/"
-    // $location.$$url = "/"
-    // $location.$$absUrl = "/"
-    // location.replace("/")
+    window.location.href = ("/logout");
   }
 
   $scope.logout = function(){
-  if (confirm('Are you sure you want to Log Out of STF?')) {
-    console.log($scope);
-    console.log($rootScope);
-    resetPaths()
-    console.log($scope);
-    console.log($rootScope);
-  } else {
-    console.log("no");
-  }
+    if (confirm('Are you sure you want to Log Out of STF?')) {
+      resetPaths()
+    } else {
+      console.log("no");
+    }
     // console.log(AppState);
+  }
+
+  $scope.init = function () {
+    $scope.activeUser = AppState.user.name
   }
 
 }

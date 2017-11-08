92662096a9a7927dd4686565e54f49bc87b70dcb
Zeglinski, Lukasz <lukasz.zeglinski@mobica.com>
2017-11-08 10:55:42 +0100

    Fix for assigning port for emulator

--- res/app/control-panes/emulator-panes-controller.js
+++ res/app/control-panes/emulator-panes-controller.js
@@ -4,8 +4,7 @@ module.exports =
     $timeout, $location, DeviceService, GroupService, ControlService,
     StorageService, FatalMessageService, SettingsService, socket) {
 
-    SettingsService.avdreset($routeParams.emulator_name, $routeParams.serial)
-
-    console.log('Emulator "'+$routeParams.serial+'" device will be restarted')
+    SettingsService.avdreset($routeParams.emulator_name, $routeParams.serial.split('-').pop(-1))
+    console.log('Emulator "'+$routeParams.serial+'" device will be restarted. Will work on "' + $routeParams.serial.split('-').pop(-1) + '" port')
     $location.path('/')
   }

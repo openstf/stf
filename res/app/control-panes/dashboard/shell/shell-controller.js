88c04fd8e242130cd987cc25f61a86a742485c3a
jakub.coufal <jakub.coufal@wandera.com>
2019-02-18 15:43:18 +0100

    Shell controller customisation

--- res/app/control-panes/dashboard/shell/shell-controller.js
+++ res/app/control-panes/dashboard/shell/shell-controller.js
@@ -22,6 +22,55 @@ module.exports = function ShellCtrl($scope) {
       })
   }
 
+  // WANDERA CUSTOM - START
+  $scope.runIntent = function(string) {
+    $scope.run('am start -d \"' + string + '\"')
+  }
+
+  $scope.runPmListPackages = function(grep) {
+    $scope.run('pm list packages | grep ' + grep)
+  }
+
+  $scope.clearDevice = function() {
+    $scope.data = 'Resetting Wandera Dev...'
+    $scope.control.shell('am start -W -a android.intent.action.VIEW -d ' +
+      "'wandera://?action=reset' com.wandera.android.dev")
+    $scope.data = 'Deauthing Wandera Dev...'
+    $scope.control.shell('am start -W -a android.intent.action.VIEW -d ' +
+      "'wandera://?action=deauthorize' com.wandera.android.dev")
+    $scope.data = 'Uninstalling Wandera Dev...'
+    $scope.control.shell('pm uninstall com.wandera.android.dev')
+    $scope.data = 'Cleaning Wandera Debug...'
+    $scope.control.shell('am start -W -a android.intent.action.VIEW -d ' +
+      "'wandera://?action=reset' com.wandera.android.debug")
+    $scope.control.shell('am start -W -a android.intent.action.VIEW -d ' +
+      "'wandera://?action=deauthorize' com.wandera.android.debug")
+    $scope.control.shell('pm uninstall com.wandera.android.debug')
+    $scope.data = 'Cleaning Wandera Prod...'
+    $scope.control.shell('am start -W -a android.intent.action.VIEW -d' +
+      " 'wandera://?action=reset' com.wandera.android")
+    $scope.control.shell('am start -W -a android.intent.action.VIEW -d' +
+      " 'wandera://?action=deauthorize' com.wandera.android")
+    $scope.control.shell('pm uninstall com.wandera.android')
+    $scope.data = 'Cleaned'
+    $scope.$digest()
+  }
+
+  $scope.disableAnimations = function() {
+    $scope.control.shell('settings put global window_animation_scale 0')
+    $scope.control.shell('settings put global transition_animation_scale 0')
+    $scope.control.shell('settings put global animator_duration_scale 0')
+    $scope.$digest()
+  }
+
+  $scope.enableAnimations = function() {
+    $scope.control.shell('settings put global window_animation_scale 1')
+    $scope.control.shell('settings put global transition_animation_scale 1')
+    $scope.control.shell('settings put global animator_duration_scale 1')
+    $scope.$digest()
+  }
+  // WANDERA CUSTOM - END
+
   $scope.clear = function() {
     $scope.command = ''
     $scope.data = ''
